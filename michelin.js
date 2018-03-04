628a495568a797b520f2455086e9f1e7657ee2d2
Karim <karimmouaddel@gmail.com>
2018-03-04 17:48:05 +0100

    I scrape name and informations from michelin page of restaurant with stars

--- michelin.js
+++ michelin.js
@@ -3,16 +3,17 @@ var cheerio = require("cheerio");
 var fs = require('fs');
 //The first page will not change so I will put this url in a const.
 const url = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin';
+const urlMich = 'https://restaurant.michelin.fr'
 
 //I try to implement the JSON file
 var jsonFile = "restaurantMichelin.json";
-var jsonContent = '{"table of restaurant" : [';
+var jsonContent = '{"restaurant michelin" : [';
 fs.writeFileSync(jsonFile,jsonContent);
 
 var number_of_pages = 1;
 
 
-function NumberOfPages(){
+function RestaurantMichelin(){
   request({ uri: url, }, function(error, resp, html){
     if(!error){
       var $ = cheerio.load(html);
@@ -24,13 +25,56 @@ function NumberOfPages(){
           var lastPage = listPage[listPage.length -2];
           number_of_pages = lastPage.children[0].attribs['attr-page-number'];
       });
-      console.log(number_of_pages);
-    }
+
+      for(var i = 1; i < number_of_pages; i++){
+        var url_pageX = url + "/page-" + i;
+        request({ uri: url, } , function(error, resp, html){
+            $ = cheerio.load(html);
+            $("a[class = poi-card-link]").each(function(){
+                var link = $(this);
+                var ref = link.attr("href");
+                var urlRestaurant = urlMich + ref;
+
+                //this could give us acces to the adress where we can find our informations
+                request({ uri: urlRestaurant, }, function(error, resp, html) {
+                    var $2 = cheerio.load(html);
+                    var name; var street; var zipCode;
+                    var city; var tel;
+
+                    name = $2("h1[itemprop=name]").text().replace("\n","");
+                    name = name.substring(6, name.length-4);
+                    name.replace("\"","");
+
+                    //I choose try catch because some restaurant don't have street or zipcode
+                    try{
+                        street = $2("div[class=thoroughfare]")[0].children[0].data;
+                    }catch(e){
+                        console.log(e);
+                    }
+
+                    try{
+                        zipCode = $2("span[class=postal-code]")[0].children[0].data;
+                    }
+                    catch(e){
+                        console.log(e);
+                    }
+
+                    city = $2("span[class=locality]")[0].children[0].data;
+                    tel = $2("div[class=tel]").text();
+
+                    //var jsonFinal = '{ "name" : "' + name + '", "street" : "' + street + '", "zipcode" : "' + zipCode + '", "city" : "' + city + '", "tel" : "' + tel + '" },\n';
+                    var jsonFinal = '{ "name" : "' + name + '", "street" : "' + street + '", "zipcode" : "' + zipCode + '", "city" : "' + city + '", "tel" : "' + tel + '" },\n';
+                    fs.appendFileSync(jsonFile,jsonFinal);
+
+                });
+              });
+            });
+          }
+        }
     else{
       console.log(error);
     }
   });
 }
 
-
-NumberOfPages();
+RestaurantMichelin();
